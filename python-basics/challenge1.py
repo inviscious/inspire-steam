cc331511bd8db20b30cfebcb9ec202085aceabbf
inviscious <jameskaruri010@gmail.com>
2026-02-11 09:09:59 +0300

    commit message

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university= "JKUAT"
+grade= "A"
+favourite_course= "cyber security"
+cluster_points= "8"
+
+print (f"I current university is {university}")
+print (f"My KCSE grade is {grade}")
+print (f"My favourite course is {favourite_course}")
+print (f"My cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+#This is a comment
+print ("Hello from James")
+# city/town
+print ("I am from Thika")
+# my hobbies
+print ("I like to read novels,star gaze,wake and bake")
+
+age = 18
+height = 1.76
+weight = 68
+
+print (f"I am {age} years old")
+print (f"I am also {height} metres tall")
+print (f"And weigh {weight} kgs")
