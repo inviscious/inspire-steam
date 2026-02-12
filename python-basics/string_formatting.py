dea016d0212363ec8d05c163c30d0c2f3dc95e2e
inviscious <jameskaruri010@gmail.com>
2026-02-12 11:09:44 +0300

    day two

---
+++ python-basics/string_formatting.py
@@ -0,0 +1,16 @@
+# James Karuri
+# Date: 11/02/2026
+# String formatting
+
+# Get string length
+sentence = "I star gaze"
+
+string_length = len(sentence)
+
+print(f"The length is: {string_length}")
+
+# Spliting a string
+sentence_2 = "Mathematics Physics"
+split = sentence_2.split(" ")
+
+print(f"the first subject is: ", split[0])
